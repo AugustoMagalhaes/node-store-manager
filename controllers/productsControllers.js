ef8f34759aed622edd4ede9b455be63d602e97bc
AugustoMagalhaes <augustommg@gmail.com>
2022-07-03 13:19:23 -0300

    chore: corrige erro de lint.

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,25 +1,33 @@
 const productsServices = require('../services/productsServices');
 
 const getAllProducts = async (_req, res) => {
+  let payload;
+  let httpStatus;
+  let error;
   try {
     const productsObj = await productsServices.getAllProducts();
-    const { payload, httpStatus, error } = productsObj;
+    ({ payload, httpStatus, error } = productsObj);
+
     if (error) throw new Error(error.message);
-    return res.status(httpStatus).json({ payload });
+
+    return res.status(httpStatus).json(payload);
   } catch (err) {
-    console.error(err);
+    return res.status(httpStatus).json({ message: err.message });
   }
 };
 
 const getProductById = async (req, res) => {
   const { id } = req.params;
+  let error;
+  let httpStatus;
+  let payload;
   try {
     const productsObj = await productsServices.getProductById(id);
-    const { error, httpStatus, payload } = productsObj;
+    ({ error, httpStatus, payload } = productsObj);
     if (error) throw new Error(error.message);
-    return res.status(httpStatus).json({ payload });
+    return res.status(httpStatus).json(payload);
   } catch (err) {
-    return res.status(httpStatus).json(err.message);
+    return res.status(httpStatus).json({ message: err.message });
   }
 };
 
