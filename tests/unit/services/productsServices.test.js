725847309900976ace6d2b360628bb47fdb018ea
AugustoMagalhaes <augustommg@gmail.com>
2022-07-03 06:49:47 -0300

    test: cria testes de productsServices.

---
+++ tests/unit/services/productsServices.test.js
@@ -0,0 +1,70 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const productsModels = require('../../../models/productsModels');
+const productsServices = require('../../../services/productsServices');
+
+describe('4 - Procura por todos produtos em services', () => {
+  before(async () => {
+    const allProducts = [
+      { id: 1, name: 'Martelo de Thor' },
+      { id: 2, name: 'Traje de encolhimento' },
+      { id: 3, name: 'Escudo do Capitão América' },
+    ];
+
+    sinon.stub(productsModels, 'getAllProducts').resolves(allProducts);
+  });
+
+  after(async () => {
+    productsModels.getAllProducts.restore();
+  });
+
+  it('Chamando todos os produtos', async () => {
+    const productsList = await productsServices.getAllProducts();
+    expect(productsList).to.be.an('array');
+    expect(productsList).to.have.lengthOf(3);
+
+    productsList.forEach((product) => {
+      expect(product).to.have.property('id');
+    });
+  });
+
+  // -------------
+});
+
+describe('5 - Procura produto por id em services', () => {
+  describe('Quando produto existe', async () => {
+    before(async () => {
+      const secProduct = [{ id: 2, name: 'Traje de encolhimento' }];
+
+      sinon.stub(productsModels, 'getProductById').resolves(secProduct);
+    });
+
+    after(async () => {
+      productsModels.getProductById.restore();
+    });
+
+    it('O retorno é um produto válido', async () => {
+      const shrinkingSuit = await productsServices.getProductById(2);
+      expect(shrinkingSuit.payload).to.have.property('id', 2);
+      expect(shrinkingSuit.payload).not.to.have.property('id', 3);
+      expect(shrinkingSuit.payload).not.to.have.property('error');
+    });
+  });
+  describe('Quando produto não existe', async () => {
+    before(async () => {
+      const cannotFind = [];
+
+      sinon.stub(productsModels, 'getProductById').resolves(cannotFind);
+    });
+
+    after(async () => {
+      productsModels.getProductById.restore();
+    });
+    it('O retorno é um objeto de erro', async () => {
+      const notFound = await productsServices.getProductById(4);
+      expect(notFound).to.be.an('object');
+      expect(notFound).to.have.property('error');
+    });
+  });
+});
