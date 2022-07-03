2ab591f88144d8dfec601bc9f4ddc2fa6ab0417a
AugustoMagalhaes <augustommg@gmail.com>
2022-07-03 12:47:56 -0300

    feat: implementa as rotas de productsRouter.

---
+++ routes/productsRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const productsRouter = express.Router();
+
+const productsController = require('../controllers/productsControllers');
+
+productsRouter.get('/', productsController.getAllProducts);
+productsRouter.get('/:id', productsController.getProductById);
+
+module.exports = productsRouter;
