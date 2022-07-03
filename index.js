d1e96d906f43d7addc8e9c06dcd4eb9ed7db73e9
AugustoMagalhaes <augustommg@gmail.com>
2022-07-03 13:20:59 -0300

    chore: inclui rotas no index.js.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const app = require('./app');
 require('dotenv').config();
 
-// não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+const morgan = require('morgan');
+const helmet = require('helmet');
+const app = require('./app');
+
+app.use(morgan);
+app.use(helmet());
+
+const productsRouter = require('./routes/productsRouter');
+
+app.use('/products', productsRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
