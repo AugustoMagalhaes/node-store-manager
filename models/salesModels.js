11b5ccdc8f23b4d40225863bb583e8af123b3ae4
AugustoMagalhaes <augustommg@gmail.com>
2022-07-12 17:21:07 -0300

    feat: implementa salesModels.

--- models/salesModels.js
+++ models/salesModels.js
@@ -1,13 +1,18 @@
 const connection = require('./connection');
 
+const getAllSales = async () => {
+  const query = `SELECT s.id as saleId, s.date, sp.product_id as productId, sp.quantity
+FROM StoreManager.sales as s
+INNER JOIN StoreManager.sales_products as sp
+ON s.id = sp.sale_id;`;
+  const [sales] = await connection.execute(query);
+  return sales;
+};
+
 const createSalesProducts = async (saleId, productId, quantity) => {
   const sp = 'sales_products';
   const query = `INSERT INTO StoreManager.${sp} (sale_id, product_id, quantity) VALUES (?, ?, ?);`;
-  const [created] = await connection.execute(query, [
-    saleId,
-    productId,
-    quantity,
-  ]);
+  const [created] = await connection.execute(query, [saleId, productId, quantity]);
   return created;
 };
 
@@ -17,4 +22,4 @@ const createSales = async () => {
   return created;
 };
 
-module.exports = { createSales, createSalesProducts };
+module.exports = { createSales, createSalesProducts, getAllSales };
