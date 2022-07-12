5c6054e2a1d521ef5a8b14a3b590e6b1c5f169e2
AugustoMagalhaes <augustommg@gmail.com>
2022-07-12 19:22:10 -0300

    feat: implementa service getSalesById.

--- services/salesServices.js
+++ services/salesServices.js
@@ -16,6 +16,27 @@ const getAllSales = async () => {
   };
 };
 
+const getSalesById = async (id) => {
+  const sales = await salesModels.getSalesById(id);
+  const properSales = sales.map((sale) => {
+    const salesEntries = Object.entries(sale);
+    salesEntries.splice(0, 1);
+    return Object.fromEntries(salesEntries);
+  });
+  if (!properSales.length) {
+    return {
+      error: {
+        message: 'Sale not found',
+      },
+      httpStatus: 404,
+    };
+  }
+  return {
+    payload: properSales,
+    httpStatus: 200,
+  };
+};
+
 const createSalesProducts = async (productsList) => {
   const { insertId } = await salesModels.createSales();
   await Promise.all(
@@ -30,4 +51,4 @@ const createSalesProducts = async (productsList) => {
   };
 };
 
-module.exports = { createSalesProducts, getAllSales };
+module.exports = { createSalesProducts, getAllSales, getSalesById };
